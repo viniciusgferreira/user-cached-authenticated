5ff8c546075d244e0c06cdab2f92e04886f34e55
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-09 13:22:54 -0300

    Verify Jwt middleware

---
+++ src/utils/verifyAuth.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+export function verifyAuth(req: Request, res: Response, next: NextFunction) {
+  const token = req.header('x-acess-token');
+
+  if (!token) { return res.status(401).json({ auth: false, message: 'Token not provided' }); }
+
+  try {
+    jwt.verify(token, String(process.env.SECRET));
+    console.log(`request authorized with token ${token}`);
+    return next();
+
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ auth: false, message: 'Token invalid' });
+  }
+}
+
