2d81b4299d8f37f85303110529f98d5365389d77
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-12 17:59:03 -0300

    show time to hash new password

--- src/services/createUser-service.ts
+++ src/services/createUser-service.ts
@@ -3,7 +3,10 @@ import { User } from '../models/User-model';
 
 export const createUserService = async (user: { username: string; password: string; }) => {
   try {
+    console.time('hash password time');
     const hashedPassword = await bcrypt.hash(user.password, 10);
+    console.timeEnd('hash password time');
+
     user.password = hashedPassword;
     return await User.create(user);
   } catch (err) {
